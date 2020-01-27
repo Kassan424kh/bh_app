91d3126d3b692273c5fd9364f83e9463e54b9ecf
kkhalil <k.khalil@satzmedia.de>
2020-01-27 17:11:49 +0100

    expanded flask restful api server

--- flask_restful_server/api.py
+++ flask_restful_server/api.py
@@ -1,21 +1,14 @@
 import json
 import ssl
-from io import StringIO
-from json import dumps, loads, load, dump
-from flask import Flask, render_template, Response, request, make_response
-from flask_restful import Resource, Api, reqparse, abort
-from datetime import datetime as dt, timedelta as td, date as d
+
+import jwt
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-from urllib.request import urlopen, Request
-from ldap3 import Server, Connection, ALL, Tls
+from ldap3 import Tls
 from flask_ldap3_login import LDAP3LoginManager
 from flask_mysqldb import MySQL
-from bson import json_util
-import datetime
 import functools
-import jwt
-from enum import Enum
-from werkzeug.security import generate_password_hash, check_password_hash
 
 with open('config.json') as json_data_file:
     ldap_login_data = json.load(json_data_file)
@@ -25,27 +18,19 @@ tls = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1,
 
 config = dict()
 
-config['LDAP_HOST'] = 'ldaps://dc02.intern.satzmedia.de' 
-
+# LDAB configurations
+config['LDAP_HOST'] = 'ldaps://dc02.intern.satzmedia.de'
 config['LDAP_PORT'] = 636
-
 config['LDAP_USE_SSL'] = True
-
 config['LDAP_USER_LOGIN_ATTR'] = 'mail'
-
 config['LDAP_BIND_USER_DN'] = ldap_login_data.get("LDAP_BIND_USER_DN", "")
-
 config['LDAP_BIND_USER_PASSWORD'] = ldap_login_data.get(
     "LDAP_BIND_USER_PASSWORD", "")
-
 config['LDAP_BASE_DN'] = 'dc=intern,dc=satzmedia,dc=de'
-
 config['LDAP_USER_DN'] = 'cn=users'
-
 config['LDAP_ADD_SERVER'] = False
 
 ldap_manager = LDAP3LoginManager()
-
 ldap_manager.init_config(config)
 
 ldap_manager.add_server(
@@ -59,7 +44,6 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -71,30 +55,41 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 
-AuthenticationResponseStatus = Enum(
-    'AuthenticationResponseStatus', 'fail success')
-
 def login_required(method):
     @functools.wraps(method)
     def wrapper(self):
-        username = request.headers.get('username', '')
+        email = request.headers.get('username', '')
         password = request.headers.get('password', '')
 
-        cur = mysql.connection.cursor()
-        # Get group by group
-        cur.execute("SELECT * FROM users WHERE `u_name` LIKE %s AND `u_password` LIKE %s;", (username, password))
+        _password = {"email": email, "password": password}
+        _encryptedPassword = jwt.encode(_password, "password", "HS256").decode('UTF-8')
 
-        users = cur.fetchall()
+        cur = mysql.connection.cursor()
+
+        cur.execute("SELECT * FROM users WHERE `email` = %s AND `u_password` = %s;", (email, _encryptedPassword))
+
+        user = cur.fetchone()
+        print(user["roll"])
 
         # Cursor close
         cur.close()
 
-        #abort(400, message='Login faild.')
-        manager = ldap_manager.authenticate(username, password)
-        isLoggedin = str(manager.status) == "AuthenticationResponseStatus.success"
-        return method(self,  isLoggedin)
-    return wrapper
+        # abort(400, message='Login faild.')
+        isLoggedin = True,  # TODO: Uncomment this ldap_manager.authenticate(email, password)
+        isNewUser = len(user) == 0
+        isAdmin = user.get("roll", -1) != -1 and user.get("roll", -1) == 2
+        isPM = user.get("roll", -1) != -1 and user.get("roll", -1) == 1
 
+        status = {}
+
+        status["isLoggedin"] = isLoggedin
+        status["isNewUser"] = isNewUser
+        status["isAdmin"] = isAdmin
+        status["isPM"] = isPM
+
+        return method(self, status)
+
+    return wrapper
 
 
 class GetUsers(Resource):
@@ -102,19 +97,11 @@ class GetUsers(Resource):
     parser.add_argument('userEmail')
 
     @login_required
+    def get(self, status):
+        return status
 
-    def get(self, isLoggedin):
-        message = ""
-        if (isLoggedin):
-            message= "you are logged in"
-        else: 
-            message= "you are not logged in"
-        return message
 
 api.add_resource(GetUsers, "/")
-
-
-
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
