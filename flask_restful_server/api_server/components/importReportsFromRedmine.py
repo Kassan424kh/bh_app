b6d790af56abd60d80d9ad360a4a547982bfc6fb
kkhalil <k.khalil@satzmedia.de>
2020-02-18 15:17:41 +0100

    fixed delete reports permanently on frontend and summarized text of reports in the same day as one reports.

--- flask_restful_server/api_server/components/importReportsFromRedmine.py
+++ flask_restful_server/api_server/components/importReportsFromRedmine.py
@@ -1,6 +1,39 @@
 import requests
 from flask_restful import abort
-from ..db.database import Database
+
+# from ..db.database import Database
+
+# example of the object coming from Redmine api
+'''
+"time_entries": [
+    ...,
+    {
+      "id": 242310,
+      "project": {
+        "id": 2,
+        "name": "5_Satzmedia"
+      },
+      "issue": {
+        "id": 40504
+      },
+      "user": {
+        "id": 333,
+        "name": "Test Test"
+      },
+      "activity": {
+        "id": 25,
+        "name": "Einarbeitung"
+      },
+      "hours": 7.0,
+      "comments": "fixed ldap server connection, use now ldap3 to authentication users, fixed rendering loop on the frontend and fixed changing animation between nullImages and showingReports",
+      "spent_on": "2020-02-17",
+      "created_on": "2020-02-17T17:41:32Z",
+      "updated_on": "2020-02-17T17:41:32Z"
+    },
+    ...,
+]
+'''
+
 
 def importReportsFromRedmineApiServerToDB() -> dict:
     key = "b869c19ea7f3d761eb7b8ce5f0bf0ddec59ced5a"
@@ -26,9 +59,29 @@ def importReportsFromRedmineApiServerToDB() -> dict:
                             user_id
                         )
                     ).json().get("time_entries", []))
-                print(len(list_of_found_reports))
 
-    return list_of_found_reports
+                # set duplicated reports together
+                checked_reports = []
+                seen = []
+
+                def checkListContaination(o) -> bool:
+                    return seen.__contains__(first_report) or checked_reports.__contains__(first_report)
+
+                for first_report in list_of_found_reports:
+                    if not checkListContaination(first_report):
+                        textOfDuplicatedReportsInTheSameDay = first_report["comments"]
+                        for second_report in list_of_found_reports:
+                            if not checkListContaination(second_report) and second_report != first_report:
+                                if first_report.get("spent_on") == second_report.get("spent_on"):
+                                    textOfDuplicatedReportsInTheSameDay += "\n" + second_report["comments"]
+                                    seen.append(second_report)
+
+                        first_report["comments"] = textOfDuplicatedReportsInTheSameDay
+
+                        seen.append(first_report)
+                        checked_reports.append(first_report)
+
+    return checked_reports
 
 
 importReportsFromRedmineApiServerToDB()
