48c52f67a06b5d7b258e43eda75b53f8955a47c2
khalil <khalilkhalil@outlook.de>
2020-03-20 14:51:20 +0100

    fixed hight of reports datatable

--- flutter_berichtsheft_app/lib/components/reports_data_table.dart
+++ flutter_berichtsheft_app/lib/components/reports_data_table.dart
@@ -132,9 +132,9 @@ class ReportsDataTable extends StatelessWidget {
             constraints: BoxConstraints(
                 maxHeight:
                     Provider.of<ReportsProvider>(context).showReportsAfterLoad
-                        ? (listOfReports.length * 60 > 600
+                        ? (listOfReports.length * (size.width > Styling.tabletSize ? 60 : 100) > 600
                                 ? 600
-                                : listOfReports.length * 60)
+                                : listOfReports.length * (size.width > Styling.tabletSize ? 60 : 100))
                             .toDouble()
                         : 0),
             child: Provider.of<ReportsProvider>(context).showReportsAfterLoad
@@ -142,7 +142,7 @@ class ReportsDataTable extends StatelessWidget {
                     child: ListView.builder(
                       itemCount: listOfReports.length,
                       cacheExtent: 10,
-                      itemExtent: size.width > Styling.phoneSize ? 60 : 100,
+                      itemExtent: size.width > Styling.tabletSize ? 60 : 100,
                       reverse: true,
                       addAutomaticKeepAlives: true,
                       physics: AlwaysScrollableScrollPhysics(),
